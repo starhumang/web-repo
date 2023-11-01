7ba3fbe7ac766585482cb10da8bb6de19f895386
admin <admin@DESKTOP-3INP67K>
2023-11-01 17:59:30 +0900

    과제있음

--- helloWeb/src/main/webapp/js/7강 DOM&EVENT/dom3.js
+++ helloWeb/src/main/webapp/js/7강 DOM&EVENT/dom3.js
@@ -5,7 +5,7 @@
 import table from './domtable.js'; //테이블 양식
 
 let url = "https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=284&serviceKey=iwO0GyTI6iNlEo5u6srMQWl6hO05L2%2Fg0d7y%2BpRVhEAgq9HIo6PXR%2F5C01FBQvdiUzMVKbpqhLjHU97D9Ei1BQ%3D%3D"
-let titles = ['센터ID', '센터명', '의료원', '연락처', '위도', '경도'];
+let titles = ['센터ID', '센터명','시도', '연락처','주소'];
 
 
 fetch(url)
@@ -46,23 +46,86 @@ function fetchCallback(result) {
         console.log(e.target.value);
         let searchSido = e.target.value;
         //선택된 시도 값에 맞는 센터명만 출력
-        genTable(rawData);
         let filterAry = rawData.filter(center => center.sido == searchSido);
         genTable(filterAry);
     }
 
-    // genTable(rawData); 초기데이터로 인덱스 값이 10 이하인 것들 화면출력     
-    let filterAry = rawData.filter((center, idx) => idx < 10);
-    genTable(filterAry);
+    genTable(rawData, ); //초기데이터로 인덱스 값이 10 이하인 것들 화면출력     
+    //let filterAry = rawData.filter((center, idx) => idx < 10);
+    //genTable(filterAry);
 }
 
 
 // 테이블 만드는 함수
-function genTable(rawData = []) {
+function genTable(rawData = [], page = 1) {
     //초기화.
     //document.getElementById("show")와 같음
     document.querySelector('#show').innerHTML = '';
+    
+    //한 페이지마다 보여줘야 할 데이터의 첫번째와 마지막을 계산하기.(page)
+    let startNo =(page -1) * 5;//인덱스랑 비교할것이기 때문에 마지막에 +1을 하지 않았음
+    let endNo = page * 5;
+    
+    
+    //[숙제] 누를때마다 옆으로 한칸씩 선택바가 이동되도록 만드시오..(5칸 중 언제나 누른게 가운데 있어야 함(1번, 2번과 마지막번호, 마지막번호에서 앞은 제외) / 앞뒤로 2개 표시 )
+    //첫번째, 마지막 페이지 = > 계산
+    let totalCnt = rawData.length;
+    let lastPage = Math.ceil(totalCnt / 5); // 전체 데이터 길이 나누기 10. 소수점아래 올림으로 계산/찐 끝 페이지
+    let endPage = Math.ceil(page / 5) * 5; // 선택 끝 페이지(선택을 10페이지씩 나오게 하겠다.)
+    let beginPage = endPage -4; //선택 시작페이지
+    let prevPage = false , nextPage = false;
+    if(beginPage > 1){//이전페이지가 있는 경우
+		prevPage = true;
+	}
+	if(endPage <lastPage){//이후페이지가 존재하는 경우
+		nextPage = true;
+	}
+    if(endPage > lastPage){
+		endPage = lastPage;
+	}
+    document.querySelector('.pagination').innerHTML = '';
+    
+    
+    //이전페이지 작성
+    if(prevPage){
+		let aTag = document.createElement('a');
+		aTag.setAttribute('href', '#');
+		aTag.innerHTML = "&laquo;";
+		aTag.addEventListener('click', function(e){
+			genTable(rawData, beginPage - 1); //이전페이지보다 1개 뺀 페이지로 이동하겠습니다
+			})
+			document.querySelector('.pagination').append(aTag);
+	}
+    
+    
+    //전체페이지 작성
+    for(let i = beginPage; i <= endPage; i++){
+		let aTag = document.createElement('a');
+		aTag.setAttribute('href', '#');
+		aTag.innerHTML = i;
+		if(i == page){
+			aTag.setAttribute('class', 'active');
+		}
+		
+		
+		aTag.addEventListener('click', function(e){
+			genTable(rawData, i);
+		})
+		document.querySelector('.pagination').append(aTag);
+	}
 
+	//이후페이지 작성
+		if(nextPage){
+			let aTag = document.createElement('a');
+			aTag.setAttribute('href', '#');
+			aTag.innerHTML = "&raquo;";
+			aTag.addEventListener('click', function(e){
+				genTable(rawData, endPage + 1);
+			})
+			document.querySelector('.pagination').append(aTag);
+		}
+		
+		
     // 전체 rawData로 출력
     let thead = table.makeHead(titles); //헤더정보
     
@@ -79,13 +142,24 @@ function genTable(rawData = []) {
 //        console.log(newCenter);
 //        return newCenter;
 //    });
+
     // [문제] 위의 mapData를 reduce로 만들기
-    	let mapData = rawData.reduce((acc, data, idx) =>{
-			
-			acc.push({id : data.id, centerName : data.centerName.replace('코로나19',''), org : data.org, phoneNumber : data.phoneNumber, lat: data.lat, lng: data.lng} )
-			console.log(acc);
-			return acc;
-		},[] );
+	let mapData = rawData.reduce((acc, center, idx) =>{
+		if(idx >= startNo && idx < endNo){
+			let newCenter = {
+	            id: center.id,
+	            centerName: center.centerName.replace('코로나19 ', ''),
+	            sido: center.sido,
+	            phoneNumber: center.phoneNumber,
+	            address : center.address,
+	            lat: center.lat,
+	            lng: center.lng
+			}		
+			acc.push(newCenter)// 새로 생성된 newCenter를 배열에 담는다.	
+        }			
+		console.log(acc);
+		return acc;
+	},[]);
     
     
     
@@ -102,10 +176,10 @@ function genTable(rawData = []) {
     let targetTr = document.querySelectorAll('tbody tr'); //tbody에 있는 tr만 가져오겠습니다.
     targetTr.forEach(tr => {
 		tr.addEventListener('click', function(e){//클릭할때 해당 메소드를 실행하겠다.
-			console.log(tr);
-			console.log(tr.children[4].innerHTML, tr.children[5].innerHTML)//innerHTML안 붙이면 앞에tr도 같이 붙어나옴
-			let lat = tr.children[4].innerHTML;//위도
-			let lng = tr.children[5].innerHTML;//경도
+			let lat = tr.dataset.lat; //let lat = tr.children[4].innerHTML;//위도  (innerHTML안 붙이면 앞에tr도 같이 붙어나옴)
+			let lng = tr.dataset.lng; //let lng = tr.children[5].innerHTML;//경도
+			
+//			console.log(tr.children[4].innerHTML, tr.children[5].innerHTML)
 //			location.href = 'daumapi.html?x=' + lat + '&y='+ lng;
 			window.open('daumapi.html?x=' + lat + '&y=' + lng);
 		})		
